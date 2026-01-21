e597b97b0b3d117668f48e3a640d186d9782fa87
Jonathan Bobel <jonathan.bobel@gsa.gov>
2026-01-21 15:04:28 -0500

    Fixing rebase issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-let siteUrl = process.env.BASEURL ? process.env.BASEURL : 'http://localhost:4321';
+let siteUrl = 'http://localhost:4321';
 if (process.env?.BRANCH === "main") {
   siteUrl = "https://councils.gov";
 } else if (process.env?.BRANCH) {
   siteUrl = `${process.env.FEDERALIST_URL}/preview/${process.env.OWNER}/${process.env.REPOSITORY}/${process.env.BRANCH}/`
+} else if (process.env.BASEURL) {
+  siteUrl = process.env.BASEURL;
 }
+
+const baseUrl = siteUrl.startsWith('http://') || siteUrl.startsWith('https://')
+  ? new URL(siteUrl).pathname
+  : (siteUrl.startsWith('/') ? siteUrl : `/${siteUrl}`);
 
 export default defineConfig({
   outDir: '_site',
   site: siteUrl,
-  base: process.env.BASEURL ? process.env.BASEURL + '/' : '/',
+  base: baseUrl.endsWith('/') ? baseUrl : baseUrl + '/',
   integrations: [react(), sitemap()],
   vite: {
     ssr: {
