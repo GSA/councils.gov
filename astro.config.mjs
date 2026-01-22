39a2b16052384ac2a1b49a429073740cab9e9ef1
Brian Hurst <brian.hurst@gsa.gov>
2026-01-22 11:18:49 -0500

    Set base config (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,30 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-let siteUrl = 'http://localhost:4321';
-if (process.env?.BRANCH === "main") {
-  siteUrl = "https://councils.gov";
-} else if (process.env?.BRANCH) {
-  siteUrl = `${process.env.FEDERALIST_URL}/preview/${process.env.OWNER}/${process.env.REPOSITORY}/${process.env.BRANCH}/`
-} else if (process.env.BASEURL) {
-  siteUrl = process.env.BASEURL;
+// If BASEURL env variable exists, update base to the BASEURL
+let base = "/";
+
+if (process.env.BASEURL) {
+  base = process.env.BASEURL;
 }
 
-const baseUrl = siteUrl.startsWith('http://') || siteUrl.startsWith('https://')
-  ? new URL(siteUrl).pathname
-  : (siteUrl.startsWith('/') ? siteUrl : `/${siteUrl}`);
-
-
 export default defineConfig({
-  outDir: '_site',
-  site: siteUrl,
-  base: baseUrl.endsWith('/') ? baseUrl : baseUrl + '/',
+  outDir: "_site",
+  site: "https://councils.gov",
+  base,
+  trailingSlash: "always",
   integrations: [react()],
   vite: {
     ssr: {
-      noExternal: ['@uswds/uswds'],
+      noExternal: ["@uswds/uswds"],
     },
     resolve: {
       alias: {
-        '@uswds/uswds/dist/css/uswds.css': '@uswds/uswds/css/uswds.css',
+        "@uswds/uswds/dist/css/uswds.css": "@uswds/uswds/css/uswds.css",
       },
     },
     build: {
-      target: 'esnext',
+      target: "esnext",
     },
   },
 });
